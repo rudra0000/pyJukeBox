54f2739355de50bebad982cb201af21259902368
rudra0000 <122379167+rudra0000@users.noreply.github.com>
2023-08-03 23:49:07 +0530

    Add files via upload

---
+++ send_email.py
@@ -0,0 +1,41 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.audio import MIMEAudio
+from email import encoders
+from email.mime.base import MIMEBase
+import magic
+sender='rudrapthk0@gmail.com'
+password='ggoo wzna xzbr wgiu'
+port=465
+def determine_mime_type(path):
+    mime=magic.Magic(mime=True)
+    return mime.from_file(path)
+def encode_audio_files(path):
+    with open(path,'rb') as f:
+        encoded_audio=f.read()
+    return encoded_audio
+def create_email(audio,sender,receiver,songName,path):
+    msg=MIMEMultipart()
+    msg['From']=sender
+    msg["To"]=receiver
+    msg['Subject']='Here are your songs'
+    mime_type=determine_mime_type(path)
+    audio_part=MIMEBase('application','octet-stream')
+    audio_part.set_payload(audio)
+    encoders.encode_base64(audio_part)
+    audio_part.add_header('Content-Disposition',f'attachment;filename="{songName}"')
+    msg.attach(audio_part)
+    return msg
+def send_email(message):
+    with smtplib.SMTP_SSL('smtp.gmail.com',port) as server:
+        server.login(sender,password)
+        server.sendmail(from_addr=sender,to_addrs='reetalec2069@gmail.com',msg=message.as_string())
+def create_email_and_send(songName,receiver):
+    path=f'/home/rudrap/pyJukebox/{songName}.mp3'
+    print(determine_mime_type(path))
+    audio=encode_audio_files(path)
+    message=create_email(audio,sender,receiver,f'{songName}.mp3',path)
+    send_email(message)
+
+if __name__=="__main__":
+    print('Sent successfully')
