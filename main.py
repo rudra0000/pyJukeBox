6897d58ad9766c0ad5f862aca02829cf11bc892a
rudra0000 <122379167+rudra0000@users.noreply.github.com>
2023-08-03 23:50:05 +0530

    Update main.py
    sending songs by mail functionality added

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from urlScrapper import get_data
 from audio_player import play_music
+from send_email import create_email_and_send
 def main():
     query=input()
+    receiver=input("Enter your email: ")
     video_url=get_data(query)
     play_music(video_url,query)
+    create_email_and_send(query,receiver)
 if __name__=='__main__':
     main()
