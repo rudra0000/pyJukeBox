c4c09149987a13f1dc7f7a509de1c72b1d79d3d2
rudra0000 <122379167+rudra0000@users.noreply.github.com>
2023-08-03 23:47:41 +0530

    Update audio_player.py
    sending songs by mail functionality added

--- audio_player.py
+++ audio_player.py
@@ -1,32 +1,24 @@
+import pytube
 from pytube import YouTube
 import vlc
 from pydub import AudioSegment
 import time
 import os
-import pygame
 def play_music(URL,query):
-    yt=YouTube(URL)
-    t=yt.streams.filter(only_audio=True).first()
-    t.download(filename=f'{query}.wav')
-    path=os.path.join(os.getcwd(),'dil.wav')
+    try:
+        yt=YouTube(URL)
+        t=yt.streams.filter(only_audio=True).first()
+        t.download(filename=f'{query}.mp3')
+    except pytube.exceptions.LiveStreamError:
+        print('Not available')
+        return
+    path=os.path.join(os.getcwd(),f'{query}.mp3')
     audio_segment=AudioSegment.from_file(path)
-    audio_duration=len(audio_segment)/1000
-    player=vlc.MediaPlayer(f'{query}.wav')
-    player.play()
-    sec=audio_duration
-    pygame.init()
-    clock=pygame.time.Clock()
-    FPS=1
-    while sec>=0:
-        print(sec)
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    player.pause()
-                    print('Paused')
-                elif event.key == pygame.K_s:
-                    player.play()
-                    print('Played')
-        if player.is_playing():
-            sec-=1
-        clock.tick(FPS)
+    # audio_duration=len(audio_segment)/1000
+    # player=vlc.MediaPlayer(f'{query}.mp3')
+    # player.play()
+    # sec=audio_duration
+    # print(audio_duration)
+    # time.sleep(audio_duration)
+
+    
