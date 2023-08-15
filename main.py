e159d8fb1bc8834f6669487add24133f03b7ed75
rudra0000 <122379167+rudra0000@users.noreply.github.com>
2023-08-15 17:55:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from urlScrapper import get_data
-from audio_player import play_music
+from audio_player import download_track
 from send_email import create_email_and_send
+from getPlaylists import get_user_tracks
 def main():
     query=input()
+    if query[0:7]=='spotify':
+        user_id=input("Enter your spotify user id ")
+        receiver=input("Enter your email: ")
+        songs=get_user_tracks(user_id)
+        for song in songs:
+            video_url=get_data(song)
+            download_track(video_url,song)
+            create_email_and_send(song,receiver)
+        return
     receiver=input("Enter your email: ")
     video_url=get_data(query)
-    play_music(video_url,query)
+    print(video_url)
+    download_track(video_url,query)
     create_email_and_send(query,receiver)
 if __name__=='__main__':
     main()
