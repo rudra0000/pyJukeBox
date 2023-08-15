893bb2417d5eaf8efe8f6772474f3ea3c84cb784
rudra0000 <122379167+rudra0000@users.noreply.github.com>
2023-08-15 17:51:49 +0530

    Update urlScrapper.py

--- urlScrapper.py
+++ urlScrapper.py
@@ -1,13 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
 import json
-API='AIzaSyC0O6p6BhEG4ym-mI0dWWSjnW4voL-F_Dc'
+from dotenv import load_dotenv
+import os
+load_dotenv()
+API=os.getenv('API')
+print(f'API is {API}')
 BASE_URL='https://www.googleapis.com/youtube/v3/search?q='
 def get_data(query):
     query+=' song'
     URL=f'{BASE_URL}{query}&key={API}&part=snippet'
     resp=requests.get(URL)
     result=resp.json()
+    print(result)
     video=result['items'][0]['id']['videoId']
     videoURL=f'https://www.youtube.com/watch?v={video}'
     return videoURL
